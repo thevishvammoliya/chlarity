2377a68bf53910a123f65f9a8e324db45b257298
Vishvam Moliya <96417040+thevishvammoliya@users.noreply.github.com>
2023-05-02 00:52:23 +0530

    manageRequests page added & working

--- pages/ngo/manageRequests.js
+++ pages/ngo/manageRequests.js
@@ -1,24 +1,128 @@
-import { NextUIProvider, Spacer, Container, Card, Grid, Text } from "@nextui-org/react";
-import NavigationBar from "../../components/NavigationBar";
+import { useState, useEffect } from "react";
+import { collection, getDocs, query, doc, updateDoc } from "firebase/firestore";
+import { db, auth } from "../../firebase-config";
+import styles from "../../CSS/ngo/manageRequests.module.css";
 import NGOlayout from "../../components/NGOlayout";
-import NGONavbar from "../../components/NGONavbar";
+import { Loading } from "@nextui-org/react";
 
-export default function Dashboard() {
-    return (
-        <>
+const ManageRequests = () => {
+    const [requests, setRequests] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const fetchRequests = async () => {
+            setLoading(true);
+            if (user) {
+                const q = query(collection(db, "requests"));
+                const requestsSnapshot = await getDocs(q);
+                const requestsList = requestsSnapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+                setRequests(requestsList);
+            }
+            setLoading(false);
+        };
+
+        fetchRequests();
+    }, [user]);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            setUser(user);
+        });
+
+        return unsubscribe;
+    }, []);
+
+    const handleApprove = async (id) => {
+        const requestRef = doc(db, "requests", id);
+        await updateDoc(requestRef, {
+            status: "Approved",
+        });
+        setRequests(requests.map((req) => (req.id === id ? { ...req, status: "Approved" } : req)));
+    };
+
+    const handleReject = async (id) => {
+        const requestRef = doc(db, "requests", id);
+        await updateDoc(requestRef, {
+            status: "Rejected",
+        });
+        setRequests(requests.map((req) => (req.id === id ? { ...req, status: "Rejected" } : req)));
+    };
+
+
+
+    if (loading) {
+        return (
             <NGOlayout>
-                <Spacer y={10} />
-                <Container justify="center" display="flex">
-                    <Card css={{ mw: "400px", p: "20px" }}>
-                        <Grid.Container gap={2} justify="center" alignContent="center">
-                            <Grid justify="center" alignItems="baseline">
-                                <Text h4>Current Balance:</Text>
-                                <Text h2 css={{ textGradient: "45deg, #000046 20%, #1CB5E0 80%" }} weight="bold">25 Ether</Text>
-                            </Grid>
-                        </Grid.Container>
-                    </Card>
-                </Container>
+                <Loading css={{ marginLeft: "50%", marginTop: "300px" }} />
             </NGOlayout>
-        </>
+        );
+    }
+
+    if (!user) {
+        return (
+            <NGOlayout>
+                <div className={styles.container}>
+                    <h1 className={styles.title}>Manage Requests</h1>
+                    <p className={styles.error}>Please login to see the details.</p>
+                </div>
+            </NGOlayout>
+        );
+    }
+
+    return (
+        <NGOlayout>
+            <div className={styles.container}>
+                <h1 className={styles.title}>Manage Requests</h1>
+                {requests.length > 0 ? (
+                    <table className={styles.table}>
+                        <thead>
+                            <tr className={styles.row}>
+                                <th className={styles.tableHead}>Patient Name</th>
+                                <th className={styles.tableHead}>Hospital Name</th>
+                                <th className={styles.tableHead}>Treatment Type</th>
+                                <th className={styles.tableHead}>Amount</th>
+                                <th className={styles.tableHead}>Status</th>
+                                <th className={styles.tableHead}>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {requests.map((request) => (
+                                <tr key={request.id} className={styles.row}>
+                                    <td className={styles.cell}>{request.patientName}</td>
+                                    <td className={styles.cell}>{request.hospitalName}</td>
+                                    <td className={styles.cell}>{request.treatmentType}</td>
+                                    <td className={styles.cell}>{request.amount}</td>
+                                    <td className={`${styles.cell} ${styles[`status-${request.status.toLowerCase()}`]}`}>
+                                        {request.status}
+                                    </td>
+                                    <td className={styles.cell}>
+                                        {request.status.toLowerCase() === "pending" && (
+                                            <>
+                                                <button className={`${styles.button} ${styles.approve}`} onClick={() => handleApprove(request.id)}>
+                                                    Approve
+                                                </button>
+                                                <button className={`${styles.button} ${styles.reject}`} onClick={() => handleReject(request.id)}>
+                                                    Reject
+                                                </button>
+                                            </>
+                                        )}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+
+
+                ) : (
+                    <p className={styles.noRequests}>No requests found.</p>
+                )}
+            </div>
+        </NGOlayout >
     );
-}
+};
+
+export default ManageRequests;
