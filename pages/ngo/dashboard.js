9157bad45f02107766bf939f5879a8e6c47cbe9d
Vishvam Moliya <96417040+thevishvammoliya@users.noreply.github.com>
2023-05-01 22:22:55 +0530

    main navbar content indicators

--- pages/ngo/dashboard.js
+++ pages/ngo/dashboard.js
@@ -1,11 +1,44 @@
-import { NextUIProvider, Spacer, Container, Card, Grid, Text } from "@nextui-org/react";
-import NavigationBar from "../../components/NavigationBar";
+import { Container, Text, Link, Input, Spacer, Loading } from "@nextui-org/react";
+import { Grid, Button, Card } from "@nextui-org/react";
+import { Component } from "react";
+import main from "../../ethereum/main"
+import web3 from "../../ethereum/web3";
 import NGOlayout from "../../components/NGOlayout";
-import NGONavbar from "../../components/NGONavbar";
 
-export default function Dashboard() {
-    return (
-        <>
+class Funds extends Component {
+
+    state = {
+        balance: '',
+        loading: true // set loading to true initially
+    }
+
+    async componentDidMount() {
+        var balance = await main.methods.getBalance().call();
+        balance = (web3.utils.fromWei(balance));
+        this.setState({ balance, loading: false }); // set loading to false after fetching the data
+    }
+
+    render() {
+        const { loading, balance } = this.state;
+
+        if (loading) {
+            return (
+                <NGOlayout>
+                    <Spacer y={10} />
+                    <Container justify="center" display="flex">
+                        <Card css={{ mw: "400px", p: "20px" }}>
+                            <Grid.Container gap={2} justify="center" alignContent="center">
+                                <Grid justify="center" alignItems="baseline">
+                                    <Loading type="points" />
+                                </Grid>
+                            </Grid.Container>
+                        </Card>
+                    </Container>
+                </NGOlayout>
+            )
+        }
+
+        return (
             <NGOlayout>
                 <Spacer y={10} />
                 <Container justify="center" display="flex">
@@ -13,12 +46,14 @@ export default function Dashboard() {
                         <Grid.Container gap={2} justify="center" alignContent="center">
                             <Grid justify="center" alignItems="baseline">
                                 <Text h4>Current Balance:</Text>
-                                <Text h2 css={{ textGradient: "45deg, #000046 20%, #1CB5E0 80%" }} weight="bold">25 Ether</Text>
+                                <Text h2 css={{ textGradient: "45deg, #000046 -30%, #1CB5E0 50%" }} weight="bold">{balance} Ether</Text>
                             </Grid>
                         </Grid.Container>
                     </Card>
                 </Container>
+
             </NGOlayout>
-        </>
-    );
+        );
+    }
 }
+export default Funds;
