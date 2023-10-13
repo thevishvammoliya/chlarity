105ea9d71cd15c60cda9256e1f9c7c6dcf22fc33
Vishvam Moliya <96417040+thevishvammoliya@users.noreply.github.com>
2023-10-13 15:01:00 +0530

    Update web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -9,7 +9,7 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    "https://goerli.infura.io/v3/0188afced4a942cd84979c3adb9c9ae7"
+    //"Your API key here,I used Infura Sepolia Testnet key"
 
   );
   web3 = new Web3(provider);
