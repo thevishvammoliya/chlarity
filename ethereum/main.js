b70c417fcfad0d5e28e70d095e7ff824c1ec0784
Vishvam Moliya <96417040+thevishvammoliya@users.noreply.github.com>
2023-10-13 15:08:34 +0530

    Update main.js

--- ethereum/main.js
+++ ethereum/main.js
@@ -3,7 +3,8 @@ import mainContract from './build/MainContract.json'
 
 const main = new web3.eth.Contract(
   mainContract.abi,
-  "0x52a451C9f72382E760dF6372fb94Ca91D4f1415e"
+  //"0x52a451C9f72382E760dF6372fb94Ca91D4f1415e"
+  //Remember, in order to use the latest deployment of the contract, you'll have to manually paste the deployed contract address in above line.
 );
 
 export default main;
